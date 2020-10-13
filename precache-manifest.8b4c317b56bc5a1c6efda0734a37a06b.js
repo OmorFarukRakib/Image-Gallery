e18d776a0f9c90029174f04706832766e1814d50
OmorFarukRakib <rakib1001@gmail.com>
2020-10-13 15:26:10 +0600

    Updates

---
+++ precache-manifest.8b4c317b56bc5a1c6efda0734a37a06b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a378949bb0ef280cdfc3a94cf8dbf9d",
+    "url": "/Image-Searching-Gallery/index.html"
+  },
+  {
+    "revision": "c16ff19bacc936d66e8b",
+    "url": "/Image-Searching-Gallery/static/css/main.bfee5d2a.chunk.css"
+  },
+  {
+    "revision": "83780c69b5c5574b9273",
+    "url": "/Image-Searching-Gallery/static/js/2.e752a839.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Image-Searching-Gallery/static/js/2.e752a839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c16ff19bacc936d66e8b",
+    "url": "/Image-Searching-Gallery/static/js/main.849149ac.chunk.js"
+  },
+  {
+    "revision": "e280945b08053133a080",
+    "url": "/Image-Searching-Gallery/static/js/runtime-main.56426c95.js"
+  }
+]);
